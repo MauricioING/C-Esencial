4ce7e582f5cd303eea2913d0d6fc1ff0f99f51b9
Mauricio <mauricio@ingenieriainversa.cl>
2024-07-17 22:04:45 -0400

    Create holaMundo.c

---
+++ holaMundo.c
@@ -0,0 +1,16 @@
+/*
+ * Curso: Integracion con Git y GitHub en Visual Studio
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+// Escribe un programa que muestre por pantalla el mensaje "Hola Mundo!"
+
+#include <stdio.h>
+
+int main(){
+
+    puts("Hola Mundo!");
+
+    return 0;
+}
